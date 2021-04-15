cd18baa623c16327c4b30d9d559c15e04d145512
Ethan Job <56394704+ethanjowb@users.noreply.github.com>
2021-04-15 12:31:01 +0800

    Vector Wont Work

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <thread>
 #include "position.hpp"
 
 int main(){
-    std::vector<int> a;
-    a[0] = 1;
-    std::cout << a[0];
+    
     return 0;
 }
